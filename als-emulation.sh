17647332af844768e69d80d07af6dc364e7262f5
Milco van Klingeren <mvklingeren@gmail.com>
2017-09-17 15:44:28 +0200

    using v4l2-ctl instead of broken cvlc from frame grabbing

--- als-emulation.sh
+++ als-emulation.sh
@@ -2,9 +2,12 @@
 # Ambient Light Sensor emulation, using a webcam
 # Copyright author: Milco van Klingeren
 # License: MIT -> keeping this copyright.
+# requires: 'light': http://haikarainen.github.io/light/
+#           and 'v4l-utils': sudo apt install v4l-utils
+#
 
-# correction factor
-correctionfactor=20
+# correction factor - tweak this for your webcam, just try 0, 2, 4, 6, 8.. etc
+correctionfactor=4.4
 
 # create a template image with a big black circle on the center (about where a person is sitting behind its laptop
 # on the webcam image : as a mask)
@@ -13,21 +16,24 @@ if [ ! -f /tmp/mask_bite.png ]; then
     echo 'mask created'
 fi
 
-# take a snapshot from the webcam
-cvlc v4l2:///dev/video0:width=640:height=480 --video-filter=scene --vout=dummy --aout=dummy --intf=dummy --scene-format=png --scene-ratio=1 --scene-prefix=snap --scene-path=/tmp vlc://quit --run-time=0.1
+# take a snapshot from the webcam, tweak using
+v4l2-ctl --device /dev/video0 --stream-mmap --stream-to=/tmp/frame.raw --stream-count=1
+convert -size 640x480 -depth 16 uyvy:/tmp/frame.raw /tmp/snap00001.png
 
 # apply the mask to our image
 composite -compose dst-out /tmp/mask_bite.png /tmp/snap00001.png -matte /tmp/snap00001.png
-alsval=$(convert /tmp/snap00001.png -colorspace gray -format "%[fx:100*mean]%%" info:|sed 's/%//'); alsval=$(bc <<<"${alsval}*2.4");
+alsval=$(convert /tmp/snap00001.png -colorspace gray -format "%[fx:100*mean]%%" info:|sed 's/%//');
+alsval=$(bc <<<"${alsval}*${correctionfactor}");
 
 #applying correction (i may bind this to the time of the day, with multiple factors in the next release)
-num=$(echo $alsval + $correctionfactor | bc)
+#num=$(echo $alsval + $correctionfactor | bc)
 
 #xbacklight is not working on ubuntu 16.04 / 17.04, with intel backlight controllers - and maybe others
 #xbacklight -set $alsval
 
 # using fabulous 'light' instead
-light -S $num
+light -S $alsval
 
-echo applying setting correction of $correctionfactor -> resulting: $num
+echo setting brightness to: $alsval
+rm /tmp/frame.raw
 rm /tmp/snap00001.png
